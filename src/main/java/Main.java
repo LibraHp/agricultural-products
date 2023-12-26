0ac6fc8446758adadc1e558c824b0897b6b45917
LibraHp_0928 <1941163264@qq.com>
2023-12-26 12:31:23 +0800

    精简main函数

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
-import com.agricultural.bean.Product;
 import com.agricultural.iframe.Login;
-import com.agricultural.service.ProductService;
-import com.agricultural.util.DatabaseUtil;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
         Login login = new Login();
     }
 }
