e50349c708e36f717a28e98e153d1e3cf00d2197
LibraHp_0928 <1941163264@qq.com>
2023-12-28 17:35:13 +0000

    update src/main/java/Main.java.
    Signed-off-by: LibraHp_0928 <1941163264@qq.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,9 @@
 import com.agricultural.iframe.Login;
-import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        try
-        {
-            BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow; // 设置窗口边框样式
-            BeautyEyeLNFHelper.launchBeautyEyeLNF();
-            UIManager.put("RootPane.setupButtonVisible", false);
-        }
-        catch(Exception e)
-        {
-            System.out.println("主题应用失败");
-        }
         Login login = new Login();
     }
 }
